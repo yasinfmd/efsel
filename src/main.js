964dd69903393e16f95532e3457b469a5ab1c230
yasinfmd <ysndlklc1234@gmail.com>
2021-01-16 12:04:06 +0300

    test

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueToast from 'vue-toast-notification';
+// Import one of the available themes
+//import 'vue-toast-notification/dist/theme-default.css';
+import 'vue-toast-notification/dist/theme-sugar.css';
+ 
+Vue.use(VueToast);
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
